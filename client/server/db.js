3f88dde76e6285fec11d28cc46544b38e919474f
RajKunx2 <113442064+RajKunx2@users.noreply.github.com>
2023-06-12 22:14:36 +0530

    update db.js

--- client/server/db.js
+++ client/server/db.js
@@ -1,22 +1,3 @@
-// const mongoose = require('mongoose');
-
-// const mongoURL =
-//   "mongodb+srv://itismerajshekhar:MR.GAMEX09479@cluster0.xoqcd5r.mongodb.net/GoFood?retryWrites=true&w=majority";
-
-// const mongoDB = async () => {
-//   await mongoose.connect(mongoURL,{ useNewUrlParser: true },async (err,result) => {
-//     if(err) console.log(err);
-//     else{
-//       console.log("connected successfully");
-//       const fetchedData = await mongoose.connection.db.collection("FOOD_ITEMS");
-//       fetchedData.find({}).toArray(function(err,data){
-//         if(err) console.log(err);
-//         else console.log(data)
-//     })
-//   }})}
-
-// module.exports = mongoDB
-
 const mongoose = require('mongoose');
 
 const mongoURL =
